30e48d7f66a9facfe566b4be96e2afa7a6e406e6
Chirag Awale <Chirag Awale>
2017-06-21 20:28:11 -0500

    Networking using Async Task

--- Networking/ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ Networking/ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -1,50 +1,78 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.android.quakereport;
 
 /**
- * Created by chira on 6/17/2017.
+ * An {@link Earthquake} object contains information related to a single earthquake.
  */
-
 public class Earthquake {
-    double mag;
-    String place,url;
-    long timeInMilliSec;
 
-    public Earthquake(double mag, String place, String url, long timeInMilliSec) {
-        this.mag = mag;
-        this.place = place;
-        this.url = url;
-        this.timeInMilliSec = timeInMilliSec;
+    /** Magnitude of the earthquake */
+    private double mMagnitude;
+
+    /** Location of the earthquake */
+    private String mLocation;
+
+    /** Time of the earthquake */
+    private long mTimeInMilliseconds;
+
+    /** Website URL of the earthquake */
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *
+     * @param magnitude is the magnitude (size) of the earthquake
+     * @param location is the location where the earthquake happened
+     * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
+     *                           earthquake happened
+     * @param url is the website URL to find more details about the earthquake
+     */
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
     }
 
-    public double getMag() {
-        return mag;
+    /**
+     * Returns the magnitude of the earthquake.
+     */
+    public double getMagnitude() {
+        return mMagnitude;
     }
 
-    public void setMag(double mag) {
-        this.mag = mag;
+    /**
+     * Returns the location of the earthquake.
+     */
+    public String getLocation() {
+        return mLocation;
     }
 
-    public String getPlace() {
-        return place;
+    /**
+     * Returns the time of the earthquake.
+     */
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
     }
 
-    public void setPlace(String place) {
-        this.place = place;
-    }
-
+    /**
+     * Returns the website URL to find more information about the earthquake.
+     */
     public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public long getTimeInMilliSec() {
-        return timeInMilliSec;
-    }
-
-    public void setTimeInMilliSec(long timeInMilliSec) {
-        this.timeInMilliSec = timeInMilliSec;
+        return mUrl;
     }
 }
